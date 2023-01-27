49e7a27f13a08c7c4cfd2e0fcbd7a6316fdaafd6
YJH2848 <yunj05763@gmail.com>
2023-01-27 23:00:12 +0900

    ADD : api호출을 할 때 동안 로딩 화면 추가

--- src/components/data.tsx
+++ src/components/data.tsx
@@ -7,14 +7,17 @@ interface Props {}
 
 const Api: React.FC<Props> = () => {
   const [state, setState] = useState<Data[]>([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetch = async () => {
       const data = await fetchData();
       setState(data);
+      setLoading(false);
     };
     fetch();
   }, []);
   console.log(state);
+
   const GRID = styled.div`
     display: grid;
     width: 100%;
@@ -31,22 +34,33 @@ const Api: React.FC<Props> = () => {
     flex-direction: column;
     align-items: center;
   `;
-
+  const Loading = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 10rem;
+    font-weight: bold;
+    height: 80vh;
+  `;
   return (
     <div>
-      <GRID>
-        {state.map((e, index) => (
-          <GRID_ITEM key={index}>
-            <div>학교이름 : {e.학교}</div>
-            <div>설립구분 : {e.설립구분}</div>
-            <div>고교유형 : {e.고교유형}</div>
-            <div>남녀공학 : {e.남녀공학구분}</div>
-            <div>학교주소 : {e.학교도로명주소}</div>
-            <div>학교번호 : {e.학교전화번호}</div>
-            <div>설립일자 : {e.설립일자}</div>
-          </GRID_ITEM>
-        ))}
-      </GRID>
+      {loading ? (
+        <Loading>Loading...</Loading>
+      ) : (
+        <GRID>
+          {state.map((e, index) => (
+            <GRID_ITEM key={index}>
+              <div>학교이름 : {e.학교}</div>
+              <div>설립구분 : {e.설립구분}</div>
+              <div>고교유형 : {e.고교유형}</div>
+              <div>남녀공학 : {e.남녀공학구분}</div>
+              <div>학교주소 : {e.학교도로명주소}</div>
+              <div>학교번호 : {e.학교전화번호}</div>
+              <div>설립일자 : {e.설립일자}</div>
+            </GRID_ITEM>
+          ))}
+        </GRID>
+      )}
     </div>
   );
 };
