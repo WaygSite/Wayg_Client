c937b5a547660278ba5e5de2f60aedc9e004e57f
YJH2848 <yunj05763@gmail.com>
2023-01-29 04:14:29 +0900

    CHORE : 아니 이걸 실수를하냐

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,35 +1,32 @@
-import React, { useState, useEffect } from "react";
-import { fetchData } from "../interface/api";
-import { Data } from "../interface/model";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-
+//styled
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 50px;
+  justify-content: space-around;
+`;
+const Input = styled.input`
+  width: 50%;
+  height: 5vh;
+  border-radius: 10px;
+  font-size: 15px;
+  padding-left: 10px;
+  &::placeholder {
+    font-size: 1rem;
+    font-weight: bolder;
+  }
+`;
 const Header: React.FC = () => {
-  const [search, setSearch] = useState<Data[]>([]);
+  const [search, setSearch] = useState([]);
   const SearchSchool = (e: any) => {
     setSearch(e.target.value);
   };
-  //styled
-  const Header = styled.div`
-    display: flex;
-    justify-content: center;
-    margin: 50px;
-    justify-content: space-around;
-  `;
-  const Input = styled.input`
-    width: 50%;
-    height: 5vh;
-    border-radius: 10px;
-    font-size: 15px;
-    padding-left: 10px;
-    &::placeholder {
-      font-size: 1rem;
-      font-weight: bolder;
-    }
-  `;
 
   return (
-    <Header>
+    <Container>
       <Link to="/">홈으로</Link>
       <Input
         placeholder="진학하고 싶은 학교를 입력해주세요"
@@ -40,7 +37,7 @@ const Header: React.FC = () => {
         <button>로그인</button>
         <button>회원가입</button>
       </div>
-    </Header>
+    </Container>
   );
 };
 
