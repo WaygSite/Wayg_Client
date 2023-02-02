db082469241f37507e50a9f405372c6ef5a218c7
YJH2848 <yunj05763@gmail.com>
2023-02-02 13:18:09 +0900

    백엔드 서버 배포

--- src/interface/api.tsx
+++ src/interface/api.tsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import { Data } from "./model";
 
 export const fetchData = async (): Promise<Data[]> => {
-  const response = await axios.get("http://localhost:3001/");
+  const response = await axios.get(
+    "https://port-0-wayg-server-sop272gldlqcwd1.gksl2.cloudtype.app/"
+  );
   return response.data.data;
 };
